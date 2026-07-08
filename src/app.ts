e3d6c0c73a1228b1c18906222ce0963cfda935cb
Peyas Barmon <dev.peyasbarmon0024@gmail.com>
2026-07-08 19:45:58 +0600

    Basic middlewares added

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response, urlencoded } from "express";
 import cors from "cors";
 import config from "./config";
+import cookieParser from "cookie-parser";
 
 const app: Application = express();
 
-app.use(cors({
+// Middlewares
+app.use(
+  cors({
     origin: config.app_url,
-    credentials: true
-}))
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// API
+app.get("/", (req: Request, res: Response) => {
+    res.send("This is Server");
+})
+
 
 export default app;
