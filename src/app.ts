8f78fea061e1d7153a0c3c3e353bafed8bf0a22a
Peyas Barmon <dev.peyasbarmon0024@gmail.com>
2026-07-20 17:59:05 +0600

    Webhook bugs

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ import { paymentController } from "./modules/payment/payment.controller";
 import { reviewRoutes } from "./modules/review/review.route";
 import { notFound } from "./middlewares/notFound";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler";
+import { sendResponse } from "./utils/sendResponse";
 
 const app: Application = express();
 
@@ -39,7 +40,10 @@ app.use(cookieParser());
 
 // API
 app.get("/", (req: Request, res: Response) => {
-  res.send("This is Server");
+  res.json({
+    status: 200,
+    message: "This is server"
+  })
 });
 
 app.use("/api/auth", authRoutes);
